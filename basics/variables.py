8a94af3ea9e9fa51afe54b6191d97fee0cb6c7f1
MuqeetVohra <muqeetvohra9@gmail.com>
2025-11-26 04:30:22 +0500

    practiced variable

--- basics/variables.py
+++ basics/variables.py
@@ -1,1 +1,13 @@
-# Hello
+# print function
+print('Practicing Python')
+print('Practicing'+' '+'Python')
+print(3)
+print(3+3)
+print('3'+'69')
+print(int('3')+int('66'))
+
+# variables
+num1 = 1
+num2 = '3'
+sum = num1 + int(num2)
+print(sum)
