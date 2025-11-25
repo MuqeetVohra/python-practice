86934598408f0f2d20700967b1c36966a5a47cbb
MuqeetVohra <muqeetvohra9@gmail.com>
2025-11-26 04:46:28 +0500

    multiple variables

--- basics/variables.py
+++ basics/variables.py
@@ -10,4 +10,10 @@ print(int('3')+int('66'))
 num1 = 1
 num2 = '3'
 sum = num1 + int(num2)
-print(sum)
+print('This sum of num1 and num2 is '+str(sum))
+
+# declaring and initializing multiple variables in a single line
+number1, number2, number3 = 3,4,5
+print(number1)
+print(number2)
+print(number3)
