3ca0ad610c60faa4c58907df2acdac030520eda9
MuqeetVohra <muqeetvohra9@gmail.com>
2025-11-27 05:18:11 +0500

    practiced arithmetic operator

---
+++ basics/operators.py
@@ -0,0 +1,29 @@
+# Operators in Python
+# 1 Arithmetic Operators
+# 2 Assignment Operators
+# 3 Comparision Operators
+# 4 Logical Operators
+# 5 Identity Operators
+# 6 Membership Operators
+
+# Arithmetic Operators
+num1 = 10
+num2 = 5
+# addition
+res = num1 + num2
+print(f'Sum is {res}')
+# subtraction
+res = num1 - num2
+print(f'Subtraction is {res}')
+# multiplication
+res = num1 * num2
+print(f'Product is {res}')
+# division
+res = num1 / num2
+print(f'Division is {res}')
+# exponent
+res = num1 ** num2
+print(f'{num1} exponent {num2} is {res}')
+# modulus give the remainder
+res = num1 % num2
+print(f'Modulus of {num1} and {num2} is {res}')
