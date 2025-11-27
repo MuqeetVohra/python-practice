2ea641272324459d7287cec94f2d3a39954c5161
MuqeetVohra <muqeetvohra9@gmail.com>
2025-11-27 05:26:56 +0500

    worked with comparision operators

--- basics/operators.py
+++ basics/operators.py
@@ -39,3 +39,15 @@ print(a)
 a /= 2
 print(a)
 b = 5
+
+# Comparision Operators
+a = 5
+b = 10
+print(a == b) # equal to
+print(a != b) # not equal to 
+print(a > b) # greater than
+print(a < b) # less than
+print(a <= b) # greater than or equal to
+print(a >= b) # less than or equal to
+
+
